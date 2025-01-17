a2b5899c90eea32a9bdfb48cbbeaf1412bb39cc5
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2025-01-17 13:50:17 +0100

    Update getPrimeFactors.js

--- algorithms/utils/getPrimeFactors.js
+++ algorithms/utils/getPrimeFactors.js
@@ -4,13 +4,15 @@
  * @returns {number[]} - An array of prime factors
  */
 export function getPrimeFactors(num) {
+    //num will go through a lot in this fuction... lol
+    //he will be judged, used, and never returned...
     let factors = [];
     let divisor = 2;
     
-    while (num >= 2) {
+    while (num >= 2) { //once divisor is greater than num... the factors gets returned
         if (num % divisor === 0) {
             factors.push(divisor);
-            num = num / divisor;
+            num = num / divisor; // num reduces to the value of dividing num by the divisor
         } else {
             divisor++;
         }
